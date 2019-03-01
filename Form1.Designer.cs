de66a697cdd640b78d6a2cb8c769e9c7645549f4
AndrewLevada <levada.an@yandex.ru>
2019-03-01 21:59:55 +0300

    all fixed and added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,97 +28,87 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(13, 13);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.textBox1.Size = new System.Drawing.Size(582, 306);
-			this.textBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(602, 13);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "LOAD";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(602, 43);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(267, 20);
-			this.textBox2.TabIndex = 3;
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(602, 69);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 4;
-			this.button2.Text = "Check";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(683, 74);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(0, 13);
-			this.label1.TabIndex = 5;
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.pictureBox1.Location = new System.Drawing.Point(602, 167);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(277, 160);
-			this.pictureBox1.TabIndex = 6;
-			this.pictureBox1.TabStop = false;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(881, 331);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.pictureBox1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(582, 306);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(602, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(602, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(277, 160);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(684, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "H";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 331);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.PictureBox pictureBox1;
-	}
+        private System.Windows.Forms.Button button1;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,14 +2,16 @@
 using System.Linq;
 using System.IO;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Citis
 {
 	public partial class Form1 : Form
 	{
 		const string fileName = "$data.txt";
-		public string[] allCities = null;
-		public string[] usedCities = null;
+        string nl = Environment.NewLine;
+        private List<string> allCities = new List<string>();
+        private List<string> usedCities = new List<string>();
 		private string LastCity = "";
 		private char LastLetter;
 		Random rnd = new Random();
@@ -22,55 +24,110 @@ namespace Citis
 
         private void Init()
 		{
-			allCities = File.ReadAllLines(fileName);
-			LastCity = allCities[rnd.Next(allCities.Length)];
+			allCities = File.ReadAllLines(fileName).ToList();
+
+			LastCity = allCities[rnd.Next(allCities.Count)];
 			LastLetter = LastCity.Last();
+
+            usedCities.Add(LastCity);
+            allCities.Remove(LastCity);
+
 			if ("ъьйёы".Contains(LastLetter))
 				LastLetter = LastCity[LastCity.Length - 2];
-			label1.Text = LastCity;
+
+            textBox1.AppendText("Компьютер говори: " + LastCity + nl);
 		}
 
-		//ДЗ
-		//Функция со списком доступных городов, вывод хода игры, ходы компьютера
-		//Встроить в чат, если будет время
+        private void CompMove()
+        {
+            List<string> useable = new List<string>();
 
-		private bool PlayerRequest(string word)
+            foreach (string e in allCities)
+                if (e.ToLower().First() == LastLetter)
+                    useable.Add(e);
+
+            if (useable.Count == 0)
+            {
+                textBox1.AppendText("Вы виграли!" + nl);
+                return;
+            }
+
+            LastCity = useable[rnd.Next(useable.Count)];
+            LastLetter = LastCity.Last();
+
+            usedCities.Add(LastCity);
+            allCities.Remove(LastCity);
+
+            if ("ъьйёы".Contains(LastLetter))
+                LastLetter = LastCity[LastCity.Length - 2];
+
+            textBox1.AppendText("Компьютер говори: " + LastCity + nl);
+
+            useable = new List<string>();
+
+            foreach (string e in allCities)
+                if (e.ToLower().First() == LastLetter)
+                    useable.Add(e);
+
+            if (useable.Count == 0)
+            {
+                textBox1.AppendText("Вы проиграли!" + nl);
+                return;
+            }
+        }
+
+		private void PlayerMove(string word)
 		{
-			if (LastCity.Last() != char.ToLower(word.First()))
-			{
-				MessageBox.Show("New word have to start from letter " + LastCity.Last());
-				return false;
-			}
+            if (!allCities.Any(c => word == c))
+            {
+                MessageBox.Show("Слово должно быть городом!");
+                return;
+            }
 
-			if (!allCities.Any(c => word == c))
+            if (usedCities.Any( str => str == word))
+            {
+                MessageBox.Show("Город уже был использован!");
+                return;
+            }
+
+            if (LastLetter != char.ToLower(word.First()))
 			{
-				MessageBox.Show("New word have to be city!");
-				return false;
+				MessageBox.Show("Новый город должен начинаться с буквы " + LastLetter + "!");
+                return;
 			}
 
 			LastCity = word;
 			LastLetter = LastCity.Last();
+
 			if ("ъьйёы".Contains(LastLetter))
 				LastLetter = LastCity[LastCity.Length - 2];
-			label1.Text = LastCity;
-			return true;
-		}
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			textBox1.Lines = allCities;
-		}
-        /// <summary>
-        /// Player wrote the word.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+            usedCities.Add(LastCity);
+            allCities.Remove(LastCity);
+
+            textBox1.AppendText("Игрок говорит: " + LastCity + nl);
+
+            CompMove();
+
+        }
+
+        //Click button "Send"
 		private void button2_Click(object sender, EventArgs e)
 		{
-			if (PlayerRequest(textBox2.Text))
-				label1.Text = "Good! Last is " + LastCity;
-			else
-				label1.Text = "No!";
-		}
-	}
+            PlayerMove(textBox2.Text);
+        }
+
+        //Click button "Help"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> useable = new List<string>();
+
+            textBox1.AppendText("Доступные города: ");
+
+            foreach (string l in allCities)
+                if (l.ToLower().First() == LastLetter)
+                    textBox1.AppendText(l + " ");
+            textBox1.AppendText(nl);
+        }
+    }
 }
